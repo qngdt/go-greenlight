fe261b92293c441ec37eabb96acef0973eae4529
qngdt <quangdt.dev@gmail.com>
2024-04-13 11:29:43 +0700

    Rate limiting

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -25,6 +25,11 @@ type config struct {
 		maxOpenConns int
 		maxIdleTime  string
 	}
+	limiter struct {
+		rps     float64
+		burst   int
+		enabled bool
+	}
 }
 
 type application struct {
@@ -41,6 +46,9 @@ func main() {
 	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("DATABASE_URL"), "PostgreSQL DSN")
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
+	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
+	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
+	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
 
 	flag.Parse()
 
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -3,13 +3,13 @@ package main
 import "net/http"
 
 func (app *application) routes() http.Handler {
-	mux := http.NewServeMux()
-	mux.HandleFunc("GET /v1/healthcheck", app.healthCheckHandler)
-	mux.HandleFunc("GET /v1/movies", app.listMovieHandler)
-	mux.HandleFunc("POST /v1/movies", app.createMovieHandler)
-	mux.HandleFunc("GET /v1/movies/{id}", app.showMovieHandler)
-	mux.HandleFunc("PATCH /v1/movies/{id}", app.updateMovieHandler)
-	mux.HandleFunc("DELETE /v1/movies/{id}", app.deleteMovieHandler)
+	router := http.NewServeMux()
+	router.HandleFunc("GET /v1/healthcheck", app.healthCheckHandler)
+	router.HandleFunc("GET /v1/movies", app.listMovieHandler)
+	router.HandleFunc("POST /v1/movies", app.createMovieHandler)
+	router.HandleFunc("GET /v1/movies/{id}", app.showMovieHandler)
+	router.HandleFunc("PATCH /v1/movies/{id}", app.updateMovieHandler)
+	router.HandleFunc("DELETE /v1/movies/{id}", app.deleteMovieHandler)
 
-	return app.recoverPanic(mux)
+	return app.recoverPanic(app.rateLimit(router))
 }
